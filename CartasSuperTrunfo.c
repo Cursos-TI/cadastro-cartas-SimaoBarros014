69f76185c95acc77be3147a2ebc1d228463df54d
SimaoBarros014 <simaobarrosoliveira554@gmail.com>
2025-09-18 03:56:11 +0000

    Primeiros Codigos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,43 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado;
+char codigodecarta [50];
+char nomedacidade [50];
+int população;
+float areaemkm²;
+float pib;
+int pontosturisticos;
   // Área para entrada de dados
+printf("Estado: \n");
+scanf("%c", &estado);
 
-  // Área para exibição dos dados da cidade
+printf("Codigo da carta: \n");
+scanf("%s", &codigodecarta);
+
+printf("Nome da cidade: \n");
+scanf("%s", &nomedacidade);
+
+printf("População: \n");
+scanf("%d", &população);
+
+printf("Area em Km²: \n");
+scanf("%f", &areaemkm²);
+
+printf("PIB: \n");
+scanf("%f", &pib);
+
+printf("Numero de pontos turisticos: \n");
+scanf("%d", &pontosturisticos);
+
+// Área para exibição dos dados da cidade
+printf("Estado: %c \n", estado);
+printf("Codigo de Carta: %s \n", codigodecarta);
+printf("Nome da Cidade: %s \n", nomedacidade);
+printf("Polulacão Civil: %d \n", população);
+printf("Area em km²: %f \n", areaemkm²);
+printf("PIB: %f \n", pib);
+printf("NUmeros de Turistas: %d \n", pontosturisticos);
 
 return 0;
 } 
